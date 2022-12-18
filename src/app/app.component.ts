6309db8abd929e04fee238bf74cc4347fc151550
Diego Camilo Herrera Ferrer <danthe1996@gmail.com>
2022-12-17 19:35:42 -0500

    V2
    v2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,36 +8,57 @@ import { ClimaService } from './services/clima.service';
 })
 export class AppComponent implements OnInit {
   titulo = 'Clima';
-  
+
   weather: any;
+  appBackColor: string = '';
+  backImg: string = "";
 
-  constructor(private climaService: ClimaService){}   
+  constructor(private climaService: ClimaService) { }
+  
+  ngOnInit(): void {
+  }
 
-  ngOnInit(){
+  ngOnChanges(): void {
     
   }
 
-  getWeather(cityName: string){
+  getWeather(cityName: string) {
+    
     this.climaService.getWeather(cityName)
       .subscribe(
         res => {
-        console.log(res);
-        this.weather= res
-        },        
+          console.log(res);
+          this.weather = res
+        },
         err => console.log(err)
       )
   }
-  
-  submitLocation(cityName: HTMLInputElement){
-  // console.log(cityName);
-  if(cityName.value){
-    this.getWeather(cityName.value)
-    cityName.value = '';    
-  } else  {
-    alert('Please, Inserte some values')
-  }  cityName.focus();
-  
-  return false;
+
+
+  submitLocation(cityName: HTMLInputElement) {
+    if (cityName.value) {
+      this.getWeather(cityName.value)
+      cityName.value = '';
+      this.fondoImg();
+      
+    } else {
+      alert('Please, Inserte some values')
+    }
+    cityName.focus();
+    // return false;
+  }
+
+  fondoImg() {
+    if (parseInt(this.weather.main.temp) < 20) {
+      this.backImg = "url('../assets/Img/cool.png')"
+      
+    } else if (parseInt(this.weather.main.temp) >= 20 && parseInt(this.weather.main.temp) < 25) {
+      this.backImg = "url('../assets/Img/Day.png')"
+      
+    } else if (parseInt(this.weather.main.temp) >= 25) {
+      this.backImg = "url('../assets/Img/hot.png')"
+      
+    }
   }
 
 
--- src/app/services/clima.service.ts
+++ src/app/services/clima.service.ts
@@ -8,10 +8,10 @@ import { Injectable } from '@angular/core';
 export class ClimaService {
   apiKey = '92f65dd45d82f9d2419f1dbf7204f4d3';
   URI: String = '';
+  URL: String = '';
   constructor(private httpClient: HttpClient) { 
     
-      this.URI =`https://api.openweathermap.org/data/2.5/weather?appid=${this.apiKey}&q=`;
-
+      this.URI =`https://api.openweathermap.org/data/2.5/weather?appid=${this.apiKey}&units=metric&q=`;
 
   }
 
